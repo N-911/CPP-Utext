a48ca5345211e58b0f0ea16a656df9651fba68a9
MSM <nikolayenkoserhiy@gmail.com>
2020-10-21 23:07:41 +0300

    move to dev

---
+++ dev/projectmanager.cpp
@@ -0,0 +1,30 @@
+#include "projectmanager.h"
+#include "loggingcategories.h"
+#include <QDir>
+#include <QFileSystemModel>
+
+ProjectManager::ProjectManager(Ui::MainWindow *parent) : m_parrent(parent) {
+}
+
+void ProjectManager::add_project_folder(const QString &dirName) {
+    qDebug(logDebug()) << "add new project " << dirName;
+    qDebug(logDebug()) << "m_parrent " << m_parrent;
+
+    if (m_dirs.count(dirName) != 0) {
+        qInfo(logInfo()) << QString("project %1 was already been open ").arg(QDir(dirName).dirName());
+        return;
+    }
+    m_dirs.push_back(dirName);
+    qInfo(logInfo()) << "m_projects size = " << m_dirs.size();
+//    m_parrent
+}
+
+void ProjectManager::close_project_folder(QModelIndex &index) {
+    // get dirname from index
+    QString dirName("find_dir");
+    qInfo(logInfo()) << index;
+//    auto it = m_projects.indexOf(dirName, 0);
+//    m_projects.remove(it);
+}
+
+
---
+++ dev/projectmanager.h
@@ -0,0 +1,36 @@
+#ifndef UTEXT_PROJECTMANAGER_H
+#define UTEXT_PROJECTMANAGER_H
+
+#include "QWidget"
+#include <QMap>
+#include <QFileSystemModel>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+    class MainWindow;
+}
+QT_END_NAMESPACE
+
+
+class ProjectManager {
+
+public:
+    ProjectManager(Ui::MainWindow *parent);
+
+//    ~ProjectManager() = default;
+
+    void add_project_folder(const QString &dirName);
+
+//    QMap<QString, QFileSystemModel*> getProjects() const;
+
+    void close_project_folder(QModelIndex& index);
+
+private:
+//    QMap<QString, QFileSystemModel*> m_projects;
+    QVector<QString> m_dirs;
+//    Ui::MainWindow *m_project_widget;
+    Ui::MainWindow *m_parrent;
+};
+
+
+#endif //UTEXT_PROJECTMANAGER_H
